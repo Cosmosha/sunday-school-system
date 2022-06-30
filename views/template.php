26ac073e0896d72a24df49da4726e36d7983e039
cosmosha <cosmoshagan@gmail.com>
2022-06-29 14:16:42 -1100

    student model modified

--- views/template.php
+++ views/template.php
@@ -26,7 +26,7 @@
 
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
-    <title>Admin Press Admin Template - The Ultimate Bootstrap 4 Admin Template</title>
+    <title>MCS System | Dashboard </title>
 
     <!-- Bootstrap Core CSS -->
     <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
