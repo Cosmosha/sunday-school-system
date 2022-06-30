f2868fa90d6ccc79d05b4a8de3f53a5f4d8621af
cosmosha <cosmoshagan@gmail.com>
2022-06-29 15:00:07 -1100

    loading methods

--- index.php
+++ index.php
@@ -7,13 +7,22 @@ require_once "controllers/teachers.controller.php";
 require_once "controllers/classrooms.controller.php";
 require_once "controllers/students.controller.php";
 
+//
+// ─── LOADING DATABASE MODEL ─────────────────────────────────────────────────────
+//
 
 require_once "models/users.model.php";
 require_once "models/teachers.model.php";
 require_once "models/classrooms.model.php";
 require_once "models/students.model.php";
 
+
+//
+// ─── LOADING METHODS ────────────────────────────────────────────────────────────
+//
+
 require_once "views/alert.php";
+require_once "views/createFolder.php";
 
 $template = new ControllerTemplate();
 $template -> ctrTemplate();
