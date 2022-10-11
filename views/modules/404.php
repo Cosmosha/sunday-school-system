42aee4023183065c958f9d76507f86514815eccb
cosmosha <cosmoshagan@gmail.com>
2022-10-11 03:57:24 +0000

    error 404

---
+++ views/modules/404.php
@@ -0,0 +1,29 @@
+
+<!-- ============================================================== -->
+<!-- Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<div class="row page-titles m-b-0">
+    <div class="col-md-5 align-self-center">
+        <h3 class="text-themecolor">Error 404</h3> 
+    </div>
+    <div class="col-md-7 align-self-center">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+            <li class="breadcrumb-item active">Error 404</li>
+        </ol>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- End Bread crumb and right sidebar toggle -->
+<!-- ============================================================== -->
+<!-- Row -->
+
+<section id="wrapper" class="error-page">
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1>404</h1>
+                <h3 class="text-uppercase">Page Not Found !</h3>
+                <p class="text-muted m-t-30 m-b-30">HOW DID YOU MANAGE TO GET HERE</p>
+                <a href="dashboards" class="btn btn-info btn-rounded waves-effect waves-light m-b-40">Back to home</a> </div>
+        </div>
+</section>
